5ff1a5144950bfc357f1754c4559b8a298c45409
highlander <bithighlander@gmail.com>
2022-03-05 14:34:33 -0700

    begin transfer example

---
+++ packages/ts-keepkey-sdk/__tests__/eth-get-address.js
@@ -0,0 +1,41 @@
+
+const { KeepKeyClient } = require("../lib")
+
+let spec = 'http://localhost:1646/spec/swagger.json'
+
+let run_test = async function () {
+    try {
+        let config = {
+            serviceKey: process.env['SERVICE_KEY'] || 'abc-123',
+            serviceName: process.env['SERVICE_NAME'] || 'KeepKey SDK Demo App',
+            serviceImageUrl: process.env['SERVICE_IMAGE_URL'] || 'https://github.com/BitHighlander/keepkey-desktop/raw/master/electron/icon.png',
+            spec
+        }
+        //init
+        let kk = await new KeepKeyClient(config).init()
+
+
+        //Unsigned TX
+        let addressInfo = {
+            addressNList: [ 2147483692, 2147483708, 2147483648, 0, 0 ],
+            coin: 'Ethereum',
+            scriptType: 'ethereum',
+            showDisplay: false
+        }
+
+        //push tx to api
+        // console.log(kk.instance.SignTransaction())
+        let timeStart = new Date().getTime()
+        let responseSign = await kk.EthGetAddress(null, addressInfo)
+        console.log("responseSign: ", responseSign.data)
+        let timeEnd = new Date().getTime()
+        console.log("duration: ",(timeStart - timeEnd) / 1000)
+        // let responseSign = await kk.instance.GetPublicKeys(null, { paths })
+        // console.log("responseSign: ", responseSign.data)
+
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+run_test()
